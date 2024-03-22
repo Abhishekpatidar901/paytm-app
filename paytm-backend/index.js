016c3f7441e29cc6c438fcb98dba3d4dec81f9f8
Abhishekpatidar901 <abhishekpatidar901@gmail.com>
2024-03-22 14:44:39 +0530

    Paytm app

---
+++ paytm-backend/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require("cors");
+const mainRouter = require("./routes/index");
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+const port = 3000;
+app.use("/api/v1", mainRouter);
+
+
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
+
+
